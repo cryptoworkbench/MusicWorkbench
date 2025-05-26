93f712aee9829e34cb6e739b7298a24f7b68fd2f
cryptoworkbench <nj.vanhoof@gmail.com>
2025-05-26 23:33:00 +0200

    big improvements

--- python/latest_code/finitering.py
+++ python/latest_code/finitering.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from help import *
 
 class NOTE(Enum): c = "C"; c_sharp = "C#"; d = "D"; d_sharp = "D#"; e = "E"; f = "F"; f_sharp = "F#"; g = "G"; g_sharp = "G#"; a = "A"; a_sharp = "A#"; b = "B";
 _c = NOTE.c; _c_sharp = NOTE.c_sharp; _d = NOTE.d; _d_sharp = NOTE.d_sharp; _e = NOTE.e; _f = NOTE.f; _f_sharp = NOTE.f_sharp;
@@ -16,27 +17,40 @@ class LL_node:
         self.content = content;
         self.next = next_node;
 
-class ring:
-    def __init__(self, values):
-        if not values: raise ValueError("Must provide at least one value.");
-        self.cardinality = 0; self.head = None; prev = None;
+def add_to_linked_list(LL_element_already_in_LL, element_to_add):
+    old_next = LL_element_already_in_LL.next;
+    LL_element_already_in_LL.next = LL_node(element_to_add);
+    LL_element_already_in_LL = LL_element_already_in_LL.next;
+    LL_element_already_in_LL.next = old_next;
+    return LL_element_already_in_LL;
 
-        # Build circular list
-        for value in values:
-            new_node = LL_node(value);
-            if not self.head:
-                self.head = new_node;
-            else:
-                prev.next = new_node;
-            prev = new_node;
+def circular_LL_from_list(list_to_convert):
+    if not list_to_convert: raise ValueError("Must provide at least one value.");
+    # ^---> check if there is at least one value in the list
+
+    head = LL_node(list_to_convert[0]); head.next = head;
+    # ^---> ifso we create a circular linked list
+
+    if len(list_to_convert) == 1: return head;
+    # ^---> if that was all we terminate here remaining_values_to_process = len(values) - 1;
+    # Build circular list
+    for i in range(1, len(list_to_convert)):
+        head = add_to_linked_list(head, list_to_convert[i]);
+    return head.next;
+
+class ring:
+    def __init__(self, circular_LL):
+        if circular_LL == None: raise ValueError("LL must be provided.");
+        self.access = circular_LL; # LL must have at least one element
+        self.cardinality = 1;
+
+        cursor = circular_LL; cursor = cursor.next;
+        while cursor != circular_LL:
+            cursor = cursor.next;
             self.cardinality += 1;
 
-        # Complete the circle
-        if prev:
-            prev.next = self.head
-
     def __iter__(self):
-        current = self.head
+        current = self.access
         count = 0
         while count < self.cardinality:
             yield current.content
@@ -46,18 +60,41 @@ class ring:
     def member_at_index(self, index):
         """Get node at a given index in the circular list (no modulo used)."""
         if index < 0: raise IndexError("Index out of bounds.");
-        ret_val = self.head;
+        ret_val = self.access;
         for _ in range(index): ret_val = ret_val.next;
         return ret_val;
 
-    def loop_from(self, starting_position):
-        cursor = self.head;
+    def loop(self, starting_position):
+        cursor = self.access; iterator = 0;
+
+        while iterator < self.cardinality and cursor != starting_position:
+            cursor = cursor.next; iterator += 1;
+        # ^---> we check to make sure the element is not literally included
+        if cursor == starting_position: # loop everything because we could the element we were looking for:
+            for i in range(self.cardinality):
+                print(cursor.content); cursor = cursor.next;
+            return;
+
+        # if code execution reached here, we didn't find the element literally, so let's look inside (search like the following:)
+        cursor = self.access; iterator = 0; # resets for searching again
+        while cursor.content != starting_position.content and iterator < self.cardinality:
+            cursor = cursor.next; iterator += 1;
+        if (iterator == self.cardinality): print("Error, object  '", starting_position.content, "'  is not in this ring !"); return;
+        for i in range(self.cardinality):
+            print(cursor.content); cursor = cursor.next;
+
+    def list_of_loop(self, starting_position):
+        ret_val = [];
+        cursor = self.access;
         iterator = 0;
         while cursor != starting_position and iterator < self.cardinality:
             cursor = cursor.next; iterator += 1;
         if (iterator == self.cardinality): print("Error, object  '", starting_position.content, "'  is not in this ring !"); return;
         for i in range(self.cardinality):
-            print(cursor.content); cursor = cursor.next;
+            print("cursor.content:", cursor.content);
+            print("cursor.content.content:", cursor.content.content, "\n");
+            ret_val.append(cursor.content.content); cursor = cursor.next;
+        return ret_val;
 
     def list_of_elements(self):
         ret_val = [];
@@ -68,20 +105,29 @@ class ring:
     def extend_with(self, value):
         """Add a new node with the given value at the end of the circular list."""
         new_node = LL_node(value)
-        if not self.head:
-            self.head = new_node
-            new_node.next = self.head
+        if not self.access:
+            self.access = new_node
+            new_node.next = self.access
             self.cardinality = 1
             return
 
-        current = self.head;
-        while current.next != self.head: current = current.next;
+        current = self.access;
+        while current.next != self.access: current = current.next;
 
         current.next = new_node
-        new_node.next = self.head
+        new_node.next = self.access
         self.cardinality += 1
 
-def traverse_ring(starting_position, distance):
+def loop_scale(starting_note):
+    read_note(starting_note);
+    looper = starting_note.next;
+    while 1:
+        if looper == starting_note:
+            break;
+        read_note(looper);
+        looper = looper.next;
+        
+def traverse_LL(starting_position, distance):
     traversed_ring = starting_position;
     for i in range(distance):
        traversed_ring = traversed_ring.next;
@@ -89,75 +135,42 @@ def traverse_ring(starting_position, distance):
 
 def read_note(note_to_read): print(note_to_read.content.value);
 def read_interval(interval_to_read): return interval_to_read.value[0];
-def apply_interval(starting_note, interval): return traverse_ring(starting_note, read_interval(interval));
+def apply_interval(starting_note, interval): return traverse_LL(starting_note, read_interval(interval));
         
 def derive_scale(root_note, mode):
-    ret_val = [root_note]; note_cursor = root_note;
-    old_head = interval_scale.head;
-    interval_scale.head = mode;
+    ret_val = [root_note.content]; note_cursor = root_note;
+    interval_scale.access = mode;
     for i, CURRENT_INTERVAL in enumerate(interval_scale):
         if i == interval_scale.cardinality - 1: break;
         new = apply_interval(note_cursor, CURRENT_INTERVAL);
-        ret_val.append(new);
+        ret_val.append(new.content);
         note_cursor = new;
-    interval_scale.head = old_head;
     return ret_val;
 
 def read_list(list_to_read):
     for list_member in list_to_read:
         print(list_member);
 
-def help():
-    print("### Help menu ( help() ):");
-    print("## ring([])");
-    print("##    Creates a finite ring (cyclical linked list) whose members are the set '[]'");
-    print("##");
-    print("## traverse_ring(starting_position, N)");
-    print("##    Returns the Nth chain of the link.");
-    print("##");
-    print("## ring.list_of_elements(ring)");
-    print("##    Returns a list containing all the elements that are in the ring (with intact order).");
-    print("##");
-    print("## ring.loop_from(object_in_ring)");
-    print("##    Loops through the entire ring, starting at object within the ring that is provided as argument.");
-    print("##    Throws an error when the object is not in the ring.");
-    print("##");
-    print("## ring.extend_with(member)");
-    print("##    Add an element to a (finite) ring.\n");
 
-chromatic_scale = ring([_c, _c_sharp, _d, _d_sharp, _e, _f, _f_sharp, _g, _g_sharp, _a, _a_sharp, _b]);
-# ^^^--> CREATE THE CHROMATIC SCALE
-
-looper = chromatic_scale.head;
-c       = looper; looper = looper.next;
-c_sharp = looper; looper = looper.next;
-d       = looper; looper = looper.next;
-d_sharp = looper; looper = looper.next;
-e       = looper; looper = looper.next;
-f       = looper; looper = looper.next;
-f_sharp = looper; looper = looper.next;
-g       = looper; looper = looper.next;
-g_sharp = looper; looper = looper.next;
-a       = looper; looper = looper.next;
-a_sharp = looper; looper = looper.next;
-b       = looper;
+chromatic_scale = ring(circular_LL_from_list([_c, _c_sharp, _d, _d_sharp, _e, _f, _f_sharp, _g, _g_sharp, _a, _a_sharp, _b])); # --> CREATE THE CHROMATIC SCALE
+looper = chromatic_scale.access;
+c       = looper; looper = looper.next; c_sharp = looper; looper = looper.next;
+d       = looper; looper = looper.next; d_sharp = looper; looper = looper.next; e       = looper; looper = looper.next;
+f       = looper; looper = looper.next; f_sharp = looper; looper = looper.next;
+g       = looper; looper = looper.next; g_sharp = looper; looper = looper.next;
+a       = looper; looper = looper.next; a_sharp = looper; looper = looper.next; b       = looper;
 # ^^^--> CREATE REFERENCES TO ALL THE NOTES
 
 half_step = INTERVAL.half_step; whole_step = INTERVAL.whole_step; minor_third = INTERVAL.minor_third; major_third = INTERVAL.major_third;
 perfect_fourth = INTERVAL.perfect_fourth; perfect_fifth = INTERVAL.perfect_fifth; tritone = INTERVAL.tritone; minor_sixth = INTERVAL.minor_sixth;
-major_sixth = INTERVAL.major_sixth; minor_seventh = INTERVAL.minor_seventh; major_seventh = INTERVAL.major_seventh;
-# ^^^--> DEFINE ALL THE INTERVALS
-
-interval_scale = ring([whole_step, whole_step, half_step, whole_step, whole_step, whole_step, half_step]);
-looper = interval_scale.head;
-ionian = looper; looper = looper.next;
-dorian = looper; looper = looper.next;
-phrygian = looper; looper = looper.next;
-lydian = looper; looper = looper.next;
-mixolydian = looper; looper = looper.next;
-aeolian = looper; looper = looper.next;
-locrian = looper;
+major_sixth = INTERVAL.major_sixth; minor_seventh = INTERVAL.minor_seventh; major_seventh = INTERVAL.major_seventh; # <<<^^^ DEFINE ALL THE INTERVALS
+interval_scale = ring(circular_LL_from_list([whole_step, whole_step, half_step, whole_step, whole_step, whole_step, half_step])); # --> CREATE THE INTERVAL SCALE
+looper = interval_scale.access;
+ionian     = looper; looper = looper.next; dorian     = looper; looper = looper.next;
+phrygian   = looper; looper = looper.next; lydian     = looper; looper = looper.next;
+mixolydian = looper; looper = looper.next; aeolian    = looper; looper = looper.next; locrian    = looper;
+# ^^^--> CREATE REFERENCES TO ALL THE MODES
 
 help()
-print("Created group 'chromatic_scale'.")
-print("Created group 'interval_scale'.")
+print("Created the ring \"chromatic_scale\", which represents the notes within an octave (C, C#, D, etc).")
+print("Created the ring \"interval_scale\", which represents all modes (ionian, dorian, etc).")
