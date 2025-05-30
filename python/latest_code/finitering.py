74fed1d6d2636a239a41a648a858d13daa5365ed
cryptoworkbench <nj.vanhoof@gmail.com>
2025-05-30 23:50:15 +0200

    nice

--- python/latest_code/finitering.py
+++ python/latest_code/finitering.py
@@ -1,3 +1,4 @@
+import os
 from enum import Enum
 from help import *
 
@@ -152,7 +153,10 @@ phrygian   = looper; looper = looper.next; lydian     = looper; looper = looper.
 mixolydian = looper; looper = looper.next; aeolian    = looper; looper = looper.next; locrian    = looper;
 # ^^^--> CREATE REFERENCES TO ALL THE MODES
 
-print("Start this program as 'python3 -i music_theory.py' if you want to get anything useful out of it.\nOnce in interactive mode, you can use 'help()' to learn about available functions.");
+pre_liminary_help_info = "Start this program as \"python3 -i ";
+pre_liminary_help_info += os.path.basename(__file__);
+pre_liminary_help_info += "\" if you want to get anything useful out of it.\nOnce in interactive mode, you can use 'help()' to learn about available functions.";
+print(pre_liminary_help_info);
 print("\nDiagnostic data:");
 print("--> created the ring 'chromatic_scale', which represents the notes within an octave (C, C#, D, etc).")
 print("--> created the ring 'interval_scale', which represents all modes (ionian, dorian, etc).")
