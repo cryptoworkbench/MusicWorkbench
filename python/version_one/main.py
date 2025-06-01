0dbd34e690becec52434b201d5aa64d829e70166
cryptoworkbench <nj.vanhoof@gmail.com>
2025-06-01 20:24:55 +0200

    remove 'version_one' and 'version_two'.

--- python/version_one/main.py
+++
@@ -1,9 +0,0 @@
-import inspect
-from enum import Enum
-from chromatic import *
-from interval import *
-
-
-print(apply_interval(c, minor_third).note.value);
-print(apply_interval(c, major_third).note.value);
-print(c.note.value);
