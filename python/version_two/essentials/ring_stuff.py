8e47ba2fd70f0b4ba6c64fa6cd53a6831b5e0604
cryptoworkbench <nj.vanhoof@gmail.com>
2025-06-07 18:25:35 +0200

    works. happy

--- python/version_two/essentials/ring_stuff.py
+++ python/version_two/essentials/ring_stuff.py
@@ -4,7 +4,7 @@ _self = importlib.import_module(__name__)
 from .notes_and_intervals.note_stuff import _NOTE, _return_NOTE_name, _return_NOTE_ColorMusic_description
 from .notes_and_intervals.interval_stuff import _INTERVAL, _return_INTERVAL_halfsteps, _return_INTERVAL_name, _return_INTERVAL_abbreviation
 from .user_utilities import *
-from .LL_node_stuff import _return_last_layer, _LL_node, _extended, _CLL_from_list, _search_CLL
+from .LL_node_stuff import _return_last_layer, _LL_node, _extended, _CLL_from_list, _search_CLL, _get_piano_note_str
 from .input_methods import *
 from .programmer_shortcuts import *
 from .musical_operations import *
@@ -179,11 +179,11 @@ class _melody(_ring):
         print(      f"{indent} The transposition '{name}' has been saved, access it like:");
         print(f"{empty_indent} {indent} {name}.loop()");
     def content(self):
-        output_str = f"{_return_last_layer(self.access)}{self.access.content.added_attribute}, "
+        output_str = f"{_get_piano_note_str(self.access)}, "
         cursor = self.access.next
-        i = 0;
+        i = 0
         while cursor and i < self.cardinality:
-            output_str += f"{_return_last_layer(cursor)}{cursor.content.added_attribute}, "
+            output_str += f"{_get_piano_note_str(cursor)}, "
             cursor = cursor.next
             i += 1
         print(output_str[:-2])
